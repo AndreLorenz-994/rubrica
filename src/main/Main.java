06a984ab2cdedb74bd472fd93d79b039f72396e0
lucagao <lucagao11@gmail.com>
2020-02-18 10:38:35 +0100

    test

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,15 +7,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-<<<<<<< HEAD
+
 		
 		ArrayList<Contatto> rubrica = new  ArrayList<Contatto>();
-		
-		
-		
-=======
->>>>>>> branch 'master' of https://github.com/AndreLorenz-994/rubrica.git
+
 	}
 	
 }
